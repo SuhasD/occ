86f469783961e5c96e505554a13615b1c38ff03a
Suhas D <suhas@suhass-mbp.echidna.com>
2016-08-31 14:28:21 +0530

    console 8

--- sw.js
+++ sw.js
@@ -18,37 +18,37 @@ self.addEventListener('install', function(e) {
 
 self.addEventListener('activate', function(e) {
   console.log('[ServiceWorker] Activate');
-  e.waitUntil(
-    caches.keys().then(function(keyList) {
-      return Promise.all(keyList.map(function(key) {
-        if (key !== cacheName) {
-          console.log('[ServiceWorker] Removing old cache', key);
-          return caches.delete(key);
-        }
-      }));
-    })
-  );
+  // e.waitUntil(
+  //   caches.keys().then(function(keyList) {
+  //     return Promise.all(keyList.map(function(key) {
+  //       if (key !== cacheName) {
+  //         console.log('[ServiceWorker] Removing old cache', key);
+  //         return caches.delete(key);
+  //       }
+  //     }));
+  //   })
+  // );
 });
 
-// self.addEventListener('fetch', function(e) {
-//   console.log('[ServiceWorker] Fetch', e.request.url);
-//   var dataUrl = 'https://publicdata-weather.firebaseio.com/';
-//   if (e.request.url.indexOf(dataUrl) === 0) {
-//     e.respondWith(
-//       fetch(e.request)
-//         .then(function(response) {
-//           return caches.open(dataCacheName).then(function(cache) {
-//             cache.put(e.request.url, response.clone());
-//             console.log('[ServiceWorker] Fetched&Cached Data');
-//             return response;
-//           });
-//         })
-//     );
-//   } else {
-//     e.respondWith(
-//       caches.match(e.request).then(function(response) {
-//         return response || fetch(e.request);
-//       })
-//     );
-//   }
-// });
+self.addEventListener('fetch', function(e) {
+  console.log('[ServiceWorker] Fetch', e.request.url);
+  var dataUrl = 'https://publicdata-weather.firebaseio.com/';
+  if (e.request.url.indexOf(dataUrl) === 0) {
+    e.respondWith(
+      fetch(e.request)
+        .then(function(response) {
+          return caches.open(dataCacheName).then(function(cache) {
+            cache.put(e.request.url, response.clone());
+            console.log('[ServiceWorker] Fetched&Cached Data');
+            return response;
+          });
+        })
+    );
+  } else {
+    e.respondWith(
+      caches.match(e.request).then(function(response) {
+        return response || fetch(e.request);
+      })
+    );
+  }
+});
