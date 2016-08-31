d3452c261f4e0bb3be8aa7162576c0ee7bb7f428
Suhas D <suhas@suhass-mbp.echidna.com>
2016-08-31 14:34:30 +0530

    console 9

--- sw.js
+++ sw.js
@@ -8,12 +8,12 @@ var filesToCache = [
 
 self.addEventListener('install', function(e) {
   console.log('[ServiceWorker] Install');
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      console.log('[ServiceWorker] Caching app shell');
-      return cache.addAll(filesToCache);
-    })
-  );
+  // e.waitUntil(
+  //   caches.open(cacheName).then(function(cache) {
+  //     console.log('[ServiceWorker] Caching app shell');
+  //     return cache.addAll(filesToCache);
+  //   })
+  // );
 });
 
 self.addEventListener('activate', function(e) {
@@ -32,23 +32,23 @@ self.addEventListener('activate', function(e) {
 
 self.addEventListener('fetch', function(e) {
   console.log('[ServiceWorker] Fetch', e.request.url);
-  var dataUrl = 'https://publicdata-weather.firebaseio.com/';
-  if (e.request.url.indexOf(dataUrl) === 0) {
-    e.respondWith(
-      fetch(e.request)
-        .then(function(response) {
-          return caches.open(dataCacheName).then(function(cache) {
-            cache.put(e.request.url, response.clone());
-            console.log('[ServiceWorker] Fetched&Cached Data');
-            return response;
-          });
-        })
-    );
-  } else {
-    e.respondWith(
-      caches.match(e.request).then(function(response) {
-        return response || fetch(e.request);
-      })
-    );
-  }
+  // var dataUrl = 'https://publicdata-weather.firebaseio.com/';
+  // if (e.request.url.indexOf(dataUrl) === 0) {
+  //   e.respondWith(
+  //     fetch(e.request)
+  //       .then(function(response) {
+  //         return caches.open(dataCacheName).then(function(cache) {
+  //           cache.put(e.request.url, response.clone());
+  //           console.log('[ServiceWorker] Fetched&Cached Data');
+  //           return response;
+  //         });
+  //       })
+  //   );
+  // } else {
+  //   e.respondWith(
+  //     caches.match(e.request).then(function(response) {
+  //       return response || fetch(e.request);
+  //     })
+  //   );
+  // }
 });
