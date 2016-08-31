781e9b39e15ad8ef73b25c28d03c3c00106700ef
Suhas D <suhas@suhass-mbp.echidna.com>
2016-08-31 14:26:02 +0530

    console 7

--- sw.js
+++ sw.js
@@ -30,25 +30,25 @@ self.addEventListener('activate', function(e) {
   );
 });
 
-self.addEventListener('fetch', function(e) {
-  console.log('[ServiceWorker] Fetch', e.request.url);
-  var dataUrl = 'https://publicdata-weather.firebaseio.com/';
-  if (e.request.url.indexOf(dataUrl) === 0) {
-    e.respondWith(
-      fetch(e.request)
-        .then(function(response) {
-          return caches.open(dataCacheName).then(function(cache) {
-            cache.put(e.request.url, response.clone());
-            console.log('[ServiceWorker] Fetched&Cached Data');
-            return response;
-          });
-        })
-    );
-  } else {
-    e.respondWith(
-      caches.match(e.request).then(function(response) {
-        return response || fetch(e.request);
-      })
-    );
-  }
-});
+// self.addEventListener('fetch', function(e) {
+//   console.log('[ServiceWorker] Fetch', e.request.url);
+//   var dataUrl = 'https://publicdata-weather.firebaseio.com/';
+//   if (e.request.url.indexOf(dataUrl) === 0) {
+//     e.respondWith(
+//       fetch(e.request)
+//         .then(function(response) {
+//           return caches.open(dataCacheName).then(function(cache) {
+//             cache.put(e.request.url, response.clone());
+//             console.log('[ServiceWorker] Fetched&Cached Data');
+//             return response;
+//           });
+//         })
+//     );
+//   } else {
+//     e.respondWith(
+//       caches.match(e.request).then(function(response) {
+//         return response || fetch(e.request);
+//       })
+//     );
+//   }
+// });
